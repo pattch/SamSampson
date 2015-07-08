4fcbba77187ec20c64fc19cc2d9e97169832fe58
Samuel <innovatests@gmail.com>
2015-07-08 16:52:30 -0700

    Design Changes
    Removed article, moving toward flex-boxes

--- scripts/main.js
+++ scripts/main.js
@@ -10,10 +10,15 @@ function mainNavigation() {
     var index = link.index();
     var prevContent = $('#content .active');
     var content = $('#content div.screen').eq(index);
+    var fadeTime = 400;
+
+    // If you click on the currently active page, do nothing
+    if(prevContent[0] === content[0])
+      return;
 
     // Fade Out Previously Active Content while Fading in New Content
-    content.addClass('active').fadeIn(200);
-    prevContent.removeClass('active').fadeOut(200);
+    content.fadeIn(400).addClass('active');
+    prevContent.fadeOut(400).removeClass('active');
     // Make Navigation indicate which page is currently active
     link.addClass('active');
     prevLink.removeClass('active');
